974e418e3e0fa7eb2ade9c34b88a03110dfead0c
DmKeller <dmit.keller@gmail.com>
2023-11-12 23:38:01 +0300

    Initial Commit

---
+++ StormViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Келлер Дмитрий on 12.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
